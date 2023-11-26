34d893e6dd742c6b512b3ff4c01febe7273d092d
Himanshu Patni <himanshupatni8.com>
2023-11-26 15:10:02 +0530

    ADDED  LOGIN ON HEADER

--- src/components/Home/HomeComponents/Header.js
+++ src/components/Home/HomeComponents/Header.js
@@ -17,6 +17,8 @@ const Header = () => {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/login">Login</Link></li>
+
         </ul>
       </nav>
     </header>
